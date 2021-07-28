ca139552f436dde054de602764772a47ee9ee9a2
Divyansh Seksaria <84166427+divyanshseksaria@users.noreply.github.com>
2021-07-28 12:08:11 +0530

    Create 04_TwoVariableInSingleLine.c

---
+++ 04_TwoVariableInSingleLine.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main() 
+{
+    int var1;
+    var1=20;
+    int var2;
+    var2=30;
+    printf("The first variable is %d and the 2nd variable is %d", var1,var2);
+    return 0;
+}
